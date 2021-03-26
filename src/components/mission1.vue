53b8673c60e4fb57241d29064936da3ee21d1687
Abdullah Khawaja <hax.3xploit@gmail.com>
2021-03-26 17:45:12 +0500

    Update mission1.vue

--- src/components/mission1.vue
+++ src/components/mission1.vue
@@ -4,11 +4,11 @@
     <div class="item">
         <div >
             <b-card-group >
-                <b-card deck v-b-hover="hoverHandler"  id="food" v-on:click=check() bg-variant="primary" text-variant="white" header="Food 🍲" class="text-center">
+                <b-card deck   id="food" v-on:click=check() bg-variant="primary" text-variant="white" header="Food 🍲" class="text-center ">
                     <b-card-text>Food is any substance consumed to provide nutritional support for an organism. </b-card-text>
                 </b-card>
 
-                <b-card v-b-hover="hoverHandler" id="electronics" bg-variant="secondary" text-variant="white" header="Electronics 🤖" class="text-center">
+                <b-card  id="electronics" bg-variant="secondary" text-variant="white" header="Electronics 🤖" class="text-center">
                     <b-card-text>Electronics comprises the physics, engineering, technology and applications that deal with the emission, flow and control of electrons in vacuum and matter.</b-card-text>
                 </b-card>
             </b-card-group>
@@ -17,11 +17,11 @@
         <div class="mt-3">
             <div >
                 <b-card-group >
-                    <b-card deck v-b-hover="hoverHandler" id="dairy" bg-variant="light" header="Dairy 🐄" class="text-center">
+                    <b-card deck  id="dairy" bg-variant="light" header="Dairy 🐄" class="text-center">
                         <b-card-text>Dairy products or milk products are a type of food produced from or containing the milk of mammals, most commonly cattle, water buffaloes, goats, sheep, and ...</b-card-text>
                     </b-card>
 
-                    <b-card deck v-b-hover="hoverHandler" id="homeDecor" bg-variant="dark" header="HomeDecor ✨" text-variant="white" class="text-center">
+                    <b-card deck  id="homeDecor" bg-variant="dark" header="HomeDecor ✨" text-variant="white" class="text-center">
                         <b-card-text>Home decor is the style of your home interiors and includes the items you use to decorate and personalise your house. </b-card-text>
                     </b-card>
                 </b-card-group>
