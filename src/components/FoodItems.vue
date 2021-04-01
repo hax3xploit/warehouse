aedb08a6e7e6c0135fa97a09e94d6b6a6ea4b2a7
hax3xploit <hax.3xploit@gmail.com>
2021-04-02 02:36:11 +0500

    init

--- src/components/FoodItems.vue
+++ src/components/FoodItems.vue
@@ -1,45 +1,50 @@
 <template>
     
 
-    <div><h1>check1</h1>
-    
-    <div  class="container" id="first"> <img  src="../assets/img/wooden-crate.png"> <img  src="../assets/img/wooden-crate.png"> </div>
-    <div  class="container" id="second">
-    <img  src="../assets/img/wooden-crate.png">
-    <img  src="../assets/img/wooden-crate.png">
-    <img  src="../assets/img/wooden-crate.png">
-    
+<div class="container mt-5">
+            <b-alert show>Search number <span id="winning_no" data-no=""></span></b-alert>
+    <div class="row">
+            <div class="col-md-3">
+                <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" class="the_box" id="box_1" name="answer" value="">
+                    <img src="../assets/img/wooden-crate.png">
+                </b-form-radio>
+            </div>
+            <div class="col-md-3">
+                <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" class="the_box" id="box_2" name="answer" value="">
+                    <img src="../assets/img/wooden-crate.png">
+                </b-form-radio>
+            </div>
+            <div class="col-md-3">
+                <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" class="the_box" id="box_3" name="answer" value="">
+                    <img src="../assets/img/wooden-crate.png">
+                </b-form-radio>
+            </div>    
+            <div class="col-md-3">
+                <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" class="the_box" id="box_4" name="answer" value="">
+                    <img src="../assets/img/wooden-crate.png">
+                </b-form-radio>
+            </div>
+            <div class="col-md-3">
+                <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" class="the_box" id="box_5" name="answer" value="">
+                    <img src="../assets/img/wooden-crate.png">
+                </b-form-radio>
+            </div>
+            <div class="col-md-3">
+                <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" class="the_box" id="box_6" name="answer" value="">
+                    <img src="../assets/img/wooden-crate.png">
+                </b-form-radio>
+            </div>
+            <div class="col-md-3">
+                <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" class="the_box" id="box_7" name="answer" value="">
+                    <img src="../assets/img/wooden-crate.png">
+                </b-form-radio>
+            </div>
+            <div class="col-md-3">
+                <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" class="the_box" id="box_8" name="answer" value="">
+                    <img src="../assets/img/wooden-crate.png">
+                </b-form-radio>
+            </div>
     </div>
-    <div class="btn">
-    <button id="b1"> Select </button>
-    <button id="b1"> Compare </button>
-    <button id="b1"> Done </button>
-    </div>
-
+</div>
     
-    
-    
-
-
-    </div>
 </template>
-<script>
-export default {
-    name:'FoodItems'
-    
-   
-
-
-    }
-
-</script>
-<style>
-
-#b1{
-   /* height: 20px;
-   width: 20px; */
-    border: 2px solid;
-    float: left;
-    margin: 10px;
-}
-</style>
--- src/components/L_linearSearch.vue
+++ src/components/L_linearSearch.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <hr>
-       <h1> Learn Linear Search</h1>
+    <h1> Learn Linear Search</h1>
     </div>
 </template>
 <script>
